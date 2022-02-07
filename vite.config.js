506165657bff4f91ef81f4110bc89dd405d5fd7e
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2022-02-07 23:09:41 +0900

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: path.resolve("src"),
+
+  build: {
+    outDir: path.resolve("dist"),
+
+    rollupOptions: {
+      input: {
+        index: path.resolve("src/index.html"),
+        foo: path.resolve("src/foo.html"),
+        nested: path.resolve("src/nested/index.html"),
+        "nested/bar": path.resolve("src/nested/bar.html"),
+      },
+    },
+  },
+});
